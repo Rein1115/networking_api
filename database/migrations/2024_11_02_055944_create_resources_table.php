46fd1e09132aef84ea70e01be8f09eac60665f05
Rein1115 <142583344+Rein1115@users.noreply.github.com>
2024-12-03 23:39:48 +0800

    UPDATE

--- database/migrations/2024_11_02_055944_create_resources_table.php
+++ database/migrations/2024_11_02_055944_create_resources_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
 
         Schema::create('resources', function (Blueprint $table) {
+            
             $table->id();
             $table->integer('code')->unique(); 
             $table->string('fname')->nullable();//done
